9c30e40b8c956fef13c78da71eb68c74ed943db1
Sagar Chada <vsagarch@adobe.com>
2018-02-23 23:22:58 -0800

    updated learn_chef_httpd cookbook

--- cookbooks/learn_chef_httpd/recipes/default.rb
+++ cookbooks/learn_chef_httpd/recipes/default.rb
@@ -4,6 +4,17 @@ service 'httpd' do
 	action [:enable, :start]
 end
 
+group 'web_admin'
+
+user 'web_admin' do
+	group 'web_admin'
+	system true
+	shell '/bin/bash'
+end
+
 template '/var/www/html/index.html' do
 	source 'index.html.erb'
+	mode '0644'
+	owner 'web_admin'
+	group 'web_admin'
 end
